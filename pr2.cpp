0a1768d20be2299934f62ade3c4852a8d30c026b
OlesMak24 <115488772+OlesMak24@users.noreply.github.com>
2022-10-20 02:46:28 +0300

    c++FILEadd

---
+++ pr2.cpp
@@ -0,0 +1,26 @@
+// Макаришин, Олесь 
+// Лабораторна робота N№ 2.1
+// Лінійні програми.
+// Варіант 23
+//sas
+#include <iostream>
+#include <cmath>
+#include <math.h>
+//dsdsdsdsd
+
+using namespace std;
+int main()
+{
+
+	cout << "a = "; cin >> a;
+//(2a - false)(2 * a - true)
+	z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * (sin(2 * a) * sin(2 * a)));
+	z2 = 2 * (sin(a));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+//slslslsl
+	cin.get();
+	return 0;
+}
