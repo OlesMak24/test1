4a4ae54f810361a9762c8983563507a44aada80d
OlesMak24 <115488772+OlesMak24@users.noreply.github.com>
2022-10-20 11:38:52 +0300

    Second Commit

---
+++ pr2.cpp
@@ -0,0 +1,29 @@
+// Макаришин, Олесь 
+// Лабораторна робота N№ 2.1
+// Лінійні програми.
+// Варіант 23
+
+#include <iostream>
+#include <cmath>
+#include <math.h>
+
+double a;
+double z1;
+double z2;
+//dsad
+using namespace std;
+int main()
+{
+
+	cout << "a = "; cin >> a;
+
+	z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * (sin(2 * a) * sin(2 * a)));
+	z2 = 2 * (sin(a));
+
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
